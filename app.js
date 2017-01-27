5008331f2ee1b3114a99895a5f93523ed3c75e6e
ComixsYT <mapr323@gmail.com>
2017-01-26 23:15:58 -0500

    First Commit
    Basic Discord bot for my server.

---
+++ app.js
@@ -0,0 +1,23 @@
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.username}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+  if (msg.content == "!live"){
+    if (msg.author.id == "145367010489008128"){
+        msg.channel.sendMessage('Hey guys, Comixs is live! http://beam.pro/comixsty, http://twitch.tv/comixsty & http://gaming.youtube.com/c/CoolComixs/live!');
+    }
+    else {
+      msg.reply('You are not a registered streamer! If you wish for your stream to be added, please message Comixs!');
+    }
+  }
+});
+
+client.login('MjcyNzU2MjgzMDM4MjM2Njcz.C2xUHA.RenWSL0Q_wVd6leerg95JQ48q7Y');
