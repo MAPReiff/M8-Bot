f0ee685107899799b19a77a0697eb70b0edb24d0
ComixsYT <mapr323@gmail.com>
2017-01-29 19:32:07 -0500

    Stream Announcements work.

--- app.js
+++ app.js
@@ -1,37 +1,52 @@
+//Invite link https://discordapp.com/oauth2/authorize?client_id=272756283038236673&scope=bot&permissions=37223488
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-var request = require('request');
-request('https://beam.pro/api/v1/channels/comixsyt', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    //console.log(body) // Show the HTML for the Google homepage.
-    var json = JSON.parse(body);
-    //console.log(json.online);
-  }
-});
-
 client.on('ready', () => {
+  //client.setPlayingGame('PornHub');
   console.log(`Logged in as ${client.user.username}!`);
 });
 
+var fs = require("fs");
+var usersRaw = fs.readFileSync("./users.txt", "utf-8");
+var users = usersRaw.split(", ");
+console.log(users);
 
+var chatsRaw = fs.readFileSync("./chats.txt", "utf-8");
+var chats = chatsRaw.split(", ");
+console.log(chats);
 
 client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
   }
-  if (msg.content == "!live"){
-    if (msg.author.id == "145367010489008128"){
-    //  msg.channel.sendMessage(channel, "Hello");
-    client.channels.get('275344557674201089').sendMessage('A message to a maybe different channel.');
-        //msg.channel.sendMessage('Hey @here, Comixs is live! http://beam.pro/comixsty, http://twitch.tv/comixsty & http://gaming.youtube.com/c/CoolComixs/live!');
-    }
-    else {
-      msg.reply('You are not a registered streamer! If you wish for your stream to be added, please message Comixs!');
-    }
+  if (msg.content.startsWith("!live")){
+    let args = msg.content.split(" ").slice(1);
+    let beam = args[0];
+    var registered = users.includes(msg.author.id);
+    if (registered == true){
+      var request = require('request');
+      request('https://beam.pro/api/v1/channels/' + beam, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+           var beamInfo = JSON.parse(body);
+           //msg.reply(beamInfo.online);
+           if (beamInfo.online == false){
+             msg.channel.sendMessage(beam + " is not live right now.")
+           }
+           if (beamInfo.online == true){
+             //msg.channel.sendMessage(beam + " is currently live @ http://beam.pro/" + beam);
+             for (i=0; i < chats.length; i++){
+               client.channels.get(chats[i]).sendMessage('@here, ' + beam + " is live @ http://beam.pro/" + beam + "!");
+             }
+           }
+        }
+      });
   }
-
+    else{
+      msg.reply('You are not a registered streamer! Please contact ComixsYT to be added.')
+    }
+}
 
 });
 
