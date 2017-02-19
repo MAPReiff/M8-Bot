bb75f260b7caeb08610ae515c1289b4f487e9486
Mitchell Reiff <me@mreiff.space>
2017-02-19 12:32:56 -0500

    Bets 2.4.5

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Invite link https://discordapp.com/oauth2/authorize?client_id=278362996349075456&scope=bot&permissions=37223488
 
-var version = "Beta 2.4.4";
+var version = "Beta 2.4.5";
 var website = "http://comixsyt.space";
 
 var fs = require("fs");
@@ -305,7 +305,7 @@ client.on("guildMemberAdd", member => {
   //console.log(guildID);
   if (guildID == "250354580926365697"){ //Chill Spot Guild ID
     member.addRole(guild.roles.find('name', 'Lil Meme'));
-    client.channels.get(guildGeneral).sendMessage("Hey " + member.displayName + ", welcome to the **Chill Spot**! You are now a Lil Meme. Please read #welcome and enjoy your stay!");
+    //client.channels.get(guildGeneral).sendMessage("Hey " + member.displayName + ", welcome to the **Chill Spot**! You are now a Lil Meme. Please read #welcome and enjoy your stay!");
   }
 });
 
