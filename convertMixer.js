00255b5b5134b3128e27a72cc81e8131cf7a1527
Mitchell Reiff <me@mreiff.space>
2019-02-03 16:36:59 -0500

    2019 Alpha 3.2
    Added converters to migrate the old streamer data

---
+++ convertMixer.js
@@ -0,0 +1,69 @@
+//Used to convert the old Mixer data to work with the 2019 rewrite
+
+const fs = require('fs')
+const streamerFolderMixer = "./streamers/mixer";
+const streamerFolderMixerOLD = "./mixer";
+const fetch = require('node-fetch')
+
+
+function convertMixer() {
+
+    console.log('This may take awhile!')
+
+    function checkStatus(res) {
+        if (res.ok) { // res.status >= 200 && res.status < 300
+            return res;
+        } else {
+            return console.log('no good')
+        }
+    }
+
+    fs.readdir(streamerFolderMixerOLD, (err, files) => {
+
+        files.forEach(file => {
+            var files = file;
+        })
+
+        var fileCount = files.length
+        for (i = 0; i < fileCount; i++) {
+            var name = files[i].replace(".txt", "");
+            if (name != ".DS_Store") {
+                console.log(name)
+                const fetch = require('node-fetch')
+
+                fetch(`https://mixer.com/api/v1/channels/${name}`)
+                    .then(checkStatus)
+                    .then(res => res.json())
+                    .then(mixerInfo => {
+
+
+                        const mixerID = mixerInfo.id;
+
+                        var guildsGood = fs.readFileSync(streamerFolderMixerOLD + `/${name}.txt`, "utf-8")
+                        var guildArray = guildsGood.split(", ")
+
+                        let defaultMixer = {
+                            name: mixerInfo.token,
+                            id: mixerInfo.id,
+                            userid: mixerInfo.userid,
+                            liveTime: '0',
+                            guilds: guildArray
+                        };
+                        let mixerJSON = JSON.stringify(defaultMixer);
+                        fs.writeFileSync(streamerFolderMixer + '/' + mixerID + '.json', mixerJSON);
+
+
+                    })
+            }
+
+        }
+
+
+    })
+
+    console.log('Finished converting Mixer Streamers!')
+
+
+}
+
+convertMixer()
---
+++ convertTwitch.js
@@ -0,0 +1,67 @@
+//Used to convert the old Twitch data to work with the 2019 rewrite
+
+
+const fs = require('fs')
+const streamerFolderTwitch = "./streamers/twitch";
+const streamerFolderTwitchOLD = "./twitch";
+const fetch = require('node-fetch')
+const config = require("./config.js");
+
+
+function convertTwitch() {
+
+    console.log('This may take awhile!')
+
+    function checkStatus(res) {
+        if (res.ok) { // res.status >= 200 && res.status < 300
+            return res;
+        } else {
+            return console.log('no good')
+        }
+    }
+
+    fs.readdir(streamerFolderTwitchOLD, (err, files) => {
+
+        files.forEach(file => {
+            var files = file;
+        })
+
+        var fileCount = files.length
+        for (i = 0; i < fileCount; i++) {
+            var name = files[i].replace(".txt", "");
+            if (name != ".DS_Store") {
+                console.log(name)
+                const fetch = require('node-fetch')
+
+                fetch(`https://api.twitch.tv/kraken/channels/${name}/?client_id=${config.twitch_id2}`)
+                    .then(checkStatus)
+                    .then(res => res.json())
+                    .then(twitchInfo => {
+
+
+                        const token = twitchInfo.display_name;
+
+                        var guildsGood = fs.readFileSync(streamerFolderTwitchOLD + `/${name}.txt`, "utf-8")
+                        var guildArray = guildsGood.split(", ")
+
+                        let defaultTwitch = {
+                            name: twitchInfo.display_name,
+                            liveTime: '0',
+                            guilds: guildArray
+                        };
+                        let twitchJSON = JSON.stringify(defaultTwitch);
+                        fs.writeFileSync(streamerFolderTwitch + '/' + token + '.json', twitchJSON);
+
+
+                    })
+            }
+
+        }
+
+    })
+
+    console.log('Finished converting Twitch Streamers!')
+
+}
+
+convertTwitch()
