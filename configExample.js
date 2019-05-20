95fafa81730a04230c68bb6013e87e71c349fb2c
Mitchell Reiff <me@mreiff.space>
2019-05-20 16:05:12 -0400

    Example Config

---
+++ configExample.js
@@ -0,0 +1,29 @@
+//Hello! Thanks for downloading the M8 Bot template!
+//Please fill in this config file, and then rename the file to 'config.js'
+
+const config = {
+    "token": "", //your bot's Discord token
+    "prefix": "-", //the default command prefix
+
+    "botSupportTeam": ['ID1', 'ID2'], //list of discord IDs for support team/staff
+
+    //API stuff
+    "discordbots_org": "", //Discordbots.org API
+
+    "twitch_id": "", //twitch ID
+    "twitch_secret": "", //twitch secret key
+
+    "twitch_id2": "", //a 2nd twitch ID  (can be the same as the first)
+    "twitch_secret2": "", //a 2nd twitch secret key (can be the same as the first)
+
+    "idiotKey": "", //idiot API key for image commands
+
+
+    //Other Stuff
+    "botLogo": "", //link to the bot's icon/logo
+    "officialDiscord": "", //link to your bot's server
+    "embedColor": "0x9900FF", //your default embed color
+
+}
+
+module.exports = config;
